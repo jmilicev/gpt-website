97bfee353929fe8ec9606afb2b713df98d373524
Jmilicev <jovan.milicev@gmail.com>
2023-06-06 18:00:40 -0400

    reconfigure for gh pages

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { call } from './caller.js';
 import Image from 'next/image';
 import './home.css';
+
+const https = require('https');
+const path = require('path');
 
 export default function Home() {
     const [inputValue, setInputValue] = useState('');
@@ -12,6 +14,161 @@ export default function Home() {
     const handleInputChange = (event) => {
       setInputValue(event.target.value);
     };
+
+    function call(input, temperature, maxTokens, modelType, PARAMETERS, apiKey, onData, onEnd) {
+      //check parameters
+    
+      var abort = false;
+      var errorlog =""
+      if(input == ""){
+        errorlog += ("ERROR: cannot have empty message\n")
+        abort = true;
+      }if(temperature>1 || temperature<0){
+        errorlog += ("ERROR: temperature must be between 0 and 1 inclusive\n");
+        abort = true;
+      }if(maxTokens<1){
+        errorlog += ("ERROR: max tokens cannot be 0 or negative\n");
+        abort = true;
+      }
+    
+    
+      const options = [
+        'gpt-4',
+        'gpt-4-0314',
+        'gpt-4-32k',
+        'gpt-4-32k-0314',
+        'gpt-3.5-turbo',
+        'gpt-3.5-turbo-0301'
+      ];
+    
+      if(!options.includes(modelType)){
+        errorlog += ("ERROR: model is not a valid option");
+        errorlog += ("options: gpt-4, gpt-4-0314, gpt-4-32k, gpt-4-32k-0314, gpt-3.5-turbo, gpt-3.5-turbo-0301");
+        abort = true;
+      }
+      
+    
+      if(!abort){
+        var rctoken = 0;
+        var trtoken = 0;
+    
+        var filebuilder = ""
+        var potentialErrorString = "";
+    
+        const startDelimiter = '{"content":"';
+        const endDelimiter = '"}';
+        const gpt35turbo_RATE = 0.000002; //per 1 token
+    
+        const req = https.request(
+          {
+            hostname: "api.openai.com",
+            port: 443,
+            path: "/v1/chat/completions",
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              'Authorization': `Bearer ${apiKey}`,
+            }
+          },
+          function (res) {
+            let responseData = '';
+    
+            res.on('data', (chunk) => {
+              responseData += chunk.toString();
+              potentialErrorString += responseData;
+    
+              while (responseData.indexOf(startDelimiter) !== -1 && responseData.indexOf(endDelimiter) !== -1) {
+                const startIndex = responseData.indexOf(startDelimiter) + startDelimiter.length;
+                const endIndex = responseData.indexOf(endDelimiter, startIndex);
+    
+                if (startIndex !== -1 && endIndex !== -1) {
+                  const content = responseData.substring(startIndex, endIndex);
+                  const output = content.replace(/\\n/g, '\n');
+                  rctoken++;
+                  filebuilder+=output;
+                  onData(output);
+                  responseData = responseData.substring(endIndex + endDelimiter.length);
+                } else {
+                  break;
+                }
+              }
+            });
+    
+            res.on('end', () => {
+              if (PARAMETERS.includes("e")) {
+                onData(" %^& END");
+              }
+    
+              if (PARAMETERS.includes("f")) {
+                saveToFile(filebuilder);
+              }
+    
+              const totaltokens = rctoken + trtoken;
+              const priceinCENTS = totaltokens * gpt35turbo_RATE * 100;
+    
+              if (rctoken === 0) {
+                const error = "\n\nERROR DETECTED: \nLIKELY API KEY / API ISSUE\n\n" + potentialErrorString + "\n\n\nEND ERROR";
+                onData(error);
+              } else if (PARAMETERS.includes("a") || PARAMETERS.includes("A")) {
+                trtoken = estimateTokenCount(input);
+                const analytics = '\n\n -- analytics --\n' +
+                  "prompt tokens spent: " + trtoken + '\n' +
+                  "completion tokens spent: " + rctoken + '\n' +
+                  "total tokens spent: " + totaltokens + '\n' +
+                  "estimated cost: ¢" + priceinCENTS.toFixed(3) + '\n' +
+                  ' ---- ---- ---- \n';
+                onData(analytics);
+              } else {
+                if (!PARAMETERS.includes("s")) {
+                  onData('\n\n ---- ---- ----  ----  ----  ---- \n\n');
+                }
+              }
+    
+              onEnd();
+            });
+          }
+        );
+    
+        req.on('error', (e) => {
+          console.error("Problem with request" + e);
+        });
+    
+        function estimateTokenCount(text) {
+          //according to OpenAI, each token is approximately 4 characters
+          return Math.round(text.length / 4);
+        }
+    
+        function processCall() {
+          var body = JSON.stringify({
+            messages: [
+              {
+                role: 'user',
+                content: input,
+              },
+            ],
+            model: modelType,
+            temperature: temperature,
+            max_tokens: maxTokens,
+            stream: true
+          });
+    
+          if (!PARAMETERS.includes("s")) {
+            onData("\n -- GPT: --\n");
+            onData("temp = " + temperature + " | m-t = " + maxTokens + " | mdl = " + modelType + "\n\n");
+          }
+          req.write(body);
+          req.end();
+        }
+    
+        processCall();
+        }else{
+          onEnd(" -- INVALID PARAMETERS | "+errorlog);
+          onData(" -- INVALID PARAMETERS | "+errorlog);
+    
+          
+        }
+    }
+    
 
     function clearChatLogs(){
         setChatLog("");
